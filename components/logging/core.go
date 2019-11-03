746214b65ae4f3e3ecf827385cfeb0ea2aa212d8
Duncan McGreggor <oubiwann@gmail.com>
2019-11-03 15:31:37 -0600

    Added logging component.

---
+++ components/logging/core.go
@@ -0,0 +1,29 @@
+package logging
+
+import (
+	"github.com/bluest-eel/state/components/config"
+	"github.com/geomyidia/zylog/logger"
+	log "github.com/sirupsen/logrus"
+)
+
+// Setup ...
+func Setup(cfg *config.Config) {
+	logger.SetupLogging(cfg.Logging)
+}
+
+// SetupClient ...
+func SetupClient(cfg *config.Config) {
+	logger.SetupLogging(cfg.Client.Logging)
+}
+
+// Load pretends that the global is more functional in nature ...
+func Load(cfg *config.Config) *log.Logger {
+	Setup(cfg)
+	return log.StandardLogger()
+}
+
+// LoadClient ...
+func LoadClient(cfg *config.Config) *log.Logger {
+	SetupClient(cfg)
+	return log.StandardLogger()
+}
