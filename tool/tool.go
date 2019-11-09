ae0b10aeb18ec1a000d5d04aca6870479c951966
Duncan McGreggor <oubiwann@gmail.com>
2019-11-09 11:14:01 -0600

    Support passing the center point as a flagged value.

--- tool/tool.go
+++ tool/tool.go
@@ -2,6 +2,9 @@
 package tool
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/golang/geo/s1"
 	"github.com/golang/geo/s2"
 	log "github.com/sirupsen/logrus"
@@ -12,41 +15,45 @@ import (
 
 const (
 	// The Earth's mean radius in kilometers (according to NASA).
-	earthRadiusKm = 6371.01
+	earthRadiusKm  = 6371.01
+	pointDelimiter = "::"
 )
 
 // Point ...
 type Point struct {
-	cellID s2.CellID
-	name   string
+	CellID s2.CellID
+	LatLon s2.LatLng
+	Name   string
 }
 
 // NewPoint ...
 func NewPoint(lat, lon float64, name string) Point {
+	latLon := s2.LatLngFromDegrees(lat, lon)
 	return Point{
-		cellID: s2.CellIDFromLatLng(s2.LatLngFromDegrees(lat, lon)),
-		name:   name,
+		CellID: s2.CellIDFromLatLng(latLon),
+		LatLon: latLon,
+		Name:   name,
 	}
 }
 
 // PointsInCellID ...
-func PointsInCellID(s2cap s2.Cap, cov s2.CellID, center s2.LatLng, points []Point) {
+func PointsInCellID(s2cap s2.Cap, cov s2.CellID, center Point, points []Point) {
 	bmin := uint64(cov.RangeMin())
 	bmax := uint64(cov.RangeMax())
 
 	for _, v := range points {
 		// This simulates an indexed range query on the DB
-		if uint64(v.cellID) < bmin || uint64(v.cellID) > bmax {
+		if uint64(v.CellID) < bmin || uint64(v.CellID) > bmax {
 			continue
 		}
 		// Only those in range
-		ll := v.cellID.LatLng()
+		ll := v.CellID.LatLng()
 		lat := ll.Lat.Degrees()
 		lon := ll.Lng.Degrees()
-		log.Infof("Nearby Candidate: %f (lat) %f (lon) %s", lat, lon, v.name)
-		log.Infof("Calculated distance to Helsinki Center: %f km",
-			AngleToKm(ll.Distance(center)))
-		log.Info("False positive? ", !s2cap.ContainsPoint(v.cellID.Point()))
+		log.Infof("Nearby Candidate: %f (lat) %f (lon) %s", lat, lon, v.Name)
+		log.Infof("Calculated distance to %s: %f (km)", center.Name,
+			AngleToKm(ll.Distance(center.LatLon)))
+		log.Info("False positive? ", !s2cap.ContainsPoint(v.CellID.Point()))
 	}
 }
 
@@ -63,13 +70,11 @@ func AngleToKm(angle s1.Angle) float64 {
 
 // Vars 'n' stuff
 var (
-	LLH = s2.LatLngFromDegrees(60.1699, 24.9384) // Helsinki Center
 	// https://www.movable-type.co.uk/scripts/latlong.html
 	Points = []Point{
-		NewPoint(60.1699, 24.9384, "Helsinki Center (0 km)"),
-		NewPoint(60.2934, 25.0378, "Vantaa Center (14.79 km)"),
-		NewPoint(60.2055, 24.6559, "Espoo Center (16.11 km)"),
-		NewPoint(60.1699, 24.9380, "Person in Helsinki (22 m)"),
+		NewPoint(60.2934, 25.0378, "Vantaa Center"),
+		NewPoint(60.2055, 24.6559, "Espoo Center"),
+		NewPoint(60.1699, 24.9380, "Person in Helsinki"),
 		NewPoint(50.0, 150.0, "far"),
 		NewPoint(150.0, 50.0, "far"),
 		NewPoint(150.0, 150.0, "far"),
@@ -78,11 +83,12 @@ var (
 )
 
 // Run https://godoc.org/github.com/golang/geo/s2#Cap
-func Run() {
-	c := s2.CellIDFromLatLng(LLH)
+func Run(center string) {
+	c := ParsePoint(center)
 	log.Infof("Center cell id: %#v", c)
+	Points = append(Points, c)
 
-	s2cap := s2.CapFromCenterAngle(c.Point(), KmToAngle(12.5))
+	s2cap := s2.CapFromCenterAngle(c.CellID.Point(), KmToAngle(12.5))
 	// http://s2geometry.io/resources/s2cell_statistics.html
 	// Level 12 are about 3 to 6.4km^2 cells
 	// Level 20 are about 46.41 to 97.3 meter cells
@@ -94,6 +100,37 @@ func Run() {
 	for i, cov := range covering {
 		log.Infof("Covering Cell %d ID: %d Level: %d", i, uint64(cov),
 			cov.Level())
-		PointsInCellID(s2cap, cov, LLH, Points)
+		PointsInCellID(s2cap, cov, c, Points)
 	}
+}
+
+// ParsePoint ...
+func ParsePoint(delimited string) Point {
+	log.Debugf("Pasring '%s' ...", delimited)
+	args := strings.Split(delimited, pointDelimiter)
+	var err error
+	var latArg, lonArg, name string
+	var lat, lon float64
+	switch {
+	case len(args) < 2:
+		log.Errorf("Bad delimited point: %s", delimited)
+		log.Fatal("At a minium, latitude and longitude must be provided")
+	case len(args) == 2:
+		latArg = args[0]
+		lonArg = args[1]
+		name = ""
+	default:
+		latArg = args[0]
+		lonArg = args[1]
+		name = args[2]
+	}
+	lat, err = strconv.ParseFloat(latArg, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lon, err = strconv.ParseFloat(lonArg, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return NewPoint(lat, lon, name)
 }
