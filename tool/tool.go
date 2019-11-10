4c3f9690c51924130f027b4e351d53bfa75b2eed
Duncan McGreggor <oubiwann@gmail.com>
2019-11-09 21:39:44 -0600

    Added support for more options and control with flags.

--- tool/tool.go
+++ tool/tool.go
@@ -19,6 +19,7 @@ const (
 	EarthRadiusKm   = 6371.01 // Earth's mean radius, km (according to NASA).
 	Helsinki        = "helsinki"
 	Oxford          = "oxford"
+	OxfordPubs      = "oxford-pubs"
 	PointDelimiter  = "::"
 	PointsDelimiter = "|"
 	Tolkien         = "tolkien"
@@ -40,19 +41,56 @@ var (
 		NewPoint(51.7572, -1.2603, "The Eagle and Child"),
 		NewPoint(51.507222, -0.1275, "London"),
 		NewPoint(51.48, 0, "Greenwich"),
-		NewPoint(50.0, 150.0, "far"),
-		NewPoint(150.0, 50.0, "far"),
-		NewPoint(150.0, 150.0, "far"),
-		NewPoint(50.0, -50.0, "far"),
+		NewPoint(52.205278, 0.119167, "Cambridge"),
 	}
 	// Note that the following are also used for tests
-	OxfordPubs = "51.7572::-1.2603::The Eagle and Child|" +
+	OxfordPubExamplePoints = "51.7572::-1.2603::The Eagle and Child|" +
 		"51.7550609::-1.2617064::Morse Bar|" +
 		"51.755::-1.2544::The King's Arms|" +
 		"51.7546135::-1.2577909::The White Horse|" +
 		"51.7547::-1.253::The Turf Tavern"
 	TolkiensHouse = "51.770903::-1.2626219::Tolkien's House"
 )
+
+/////////////////////////////////////////////////////////////////////////////
+///   Tool Entrypouint   /////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
+// RunOptions ...
+type RunOptions struct {
+	CapRadius   float64
+	CenterPoint string
+	MaxCells    int
+	MaxLevel    int
+	Points      string
+}
+
+// Run https://godoc.org/github.com/golang/geo/s2#Cap
+func Run(opts *RunOptions) {
+	center := ParsePoint(opts.CenterPoint)
+	log.Infof("Center cell id: %#v", center)
+	points := ParsePoints(opts.Points)
+	points = append(points, center)
+
+	s2cap := s2.CapFromCenterAngle(center.CellID.Point(), KmToAngle(opts.CapRadius))
+	// http://s2geometry.io/resources/s2cell_statistics.html
+	// Level 12 are about 3 to 6.4km^2 cells
+	// Level 20 are about 46.41 to 97.3 meter cells
+	// Since we put a MaxCells of 8, it won't go to the max level if it
+	// can't approximate the region better anyway.
+	rc := &s2.RegionCoverer{MaxLevel: opts.MaxLevel, MaxCells: opts.MaxCells}
+	covering := rc.Covering(s2.Region(s2cap))
+
+	for i, cov := range covering {
+		log.Infof("Covering Cell %d ID: %d Level: %d", i, uint64(cov),
+			cov.Level())
+		PointsInCellID(s2cap, cov, center, points)
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////
+///   Point Implementation   ////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 
 // Point ...
 type Point struct {
@@ -71,47 +109,67 @@ func NewPoint(lat, lon float64, name string) Point {
 	}
 }
 
+/////////////////////////////////////////////////////////////////////////////
+///   Points Functions   ////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
 // PointsInCellID ...
 func PointsInCellID(s2cap s2.Cap, cov s2.CellID, center Point, points []Point) {
 	bmin := uint64(cov.RangeMin())
 	bmax := uint64(cov.RangeMax())
 
-	for _, v := range points {
+	for _, p := range points {
 		// This simulates an indexed range query on the DB
-		if uint64(v.CellID) < bmin || uint64(v.CellID) > bmax {
+		if uint64(p.CellID) < bmin || uint64(p.CellID) > bmax {
 			continue
 		}
 		// Only those in range
-		ll := v.CellID.LatLng()
-		lat := ll.Lat.Degrees()
-		lon := ll.Lng.Degrees()
-		log.Infof("Nearby Candidate: %f (lat) %f (lon) %s", lat, lon, v.Name)
+		lat := p.LatLon.Lat.Degrees()
+		lon := p.LatLon.Lng.Degrees()
+		log.Infof("Nearby Candidate: %f (lat) %f (lon) %s", lat, lon, p.Name)
 		log.Infof("Calculated distance to %s: %f (km)", center.Name,
-			AngleToKm(ll.Distance(center.LatLon)))
-		log.Info("False positive? ", !s2cap.ContainsPoint(v.CellID.Point()))
+			AngleToKm(p.LatLon.Distance(center.LatLon)))
+		log.Info("False positive? ", !s2cap.ContainsPoint(p.CellID.Point()))
 	}
 }
 
-// Run https://godoc.org/github.com/golang/geo/s2#Cap
-func Run(c string, p string) {
-	center := ParsePoint(c)
-	log.Infof("Center cell id: %#v", center)
-	points := ParsePoints(p)
-	points = append(points, center)
+/////////////////////////////////////////////////////////////////////////////
+///   CLI Helper Functions   ////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 
-	s2cap := s2.CapFromCenterAngle(center.CellID.Point(), KmToAngle(12.5))
-	// http://s2geometry.io/resources/s2cell_statistics.html
-	// Level 12 are about 3 to 6.4km^2 cells
-	// Level 20 are about 46.41 to 97.3 meter cells
-	// Since we put a MaxCells of 8, it won't go to the max level if it
-	// can't approximate the region better anyway.
-	rc := &s2.RegionCoverer{MaxLevel: 20, MaxCells: 8}
-	covering := rc.Covering(s2.Region(s2cap))
+// ParsePoint ...
+func ParsePoint(delimited string) Point {
+	log.Debugf("Pasring point '%s' ...", delimited)
+	var latArg, lonArg, name string
+	err := Splitter(delimited, PointSplitterOpts, &latArg, &lonArg, &name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if latArg == Tolkien {
+		err = Splitter(TolkiensHouse, PointSplitterOpts, &latArg, &lonArg, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	lat, lon := ConvertLatLon(latArg, lonArg)
+	return NewPoint(lat, lon, name)
+}
 
-	for i, cov := range covering {
-		log.Infof("Covering Cell %d ID: %d Level: %d", i, uint64(cov),
-			cov.Level())
-		PointsInCellID(s2cap, cov, center, points)
+// ParsePoints ...
+func ParsePoints(delimited string) []Point {
+	log.Debugf("Pasring points '%s' ...", delimited)
+	args := strings.Split(delimited, PointsDelimiter)
+	switch {
+	case args[0] == Helsinki:
+		return HelsinkiExamplePoints
+	case args[0] == Oxford:
+		return OxfordExamplePoints
+	default:
+		points := make([]Point, len(args))
+		for i, p := range args {
+			points[i] = ParsePoint(p)
+		}
+		return points
 	}
 }
 
@@ -162,50 +220,15 @@ func Splitter(s string, opts *SplitterOpts, vars ...*string) error {
 	return nil
 }
 
-/////////////////////////////////////////////////////////////////////////////
-///   CLI Helper Functions   ////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////
-
-// ParsePoint ...
-func ParsePoint(delimited string) Point {
-	log.Debugf("Pasring point '%s' ...", delimited)
-	var latArg, lonArg, name string
-	err := Splitter(delimited, PointSplitterOpts, &latArg, &lonArg, &name)
+// ConvertLatLon ...
+func ConvertLatLon(lat, lon string) (float64, float64) {
+	lat64, err := strconv.ParseFloat(lat, 64)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if latArg == Tolkien {
-		err = Splitter(TolkiensHouse, PointSplitterOpts, &latArg, &lonArg, &name)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	var lat, lon float64
-	lat, err = strconv.ParseFloat(latArg, 64)
+	lon64, err := strconv.ParseFloat(lon, 64)
 	if err != nil {
 		log.Fatal(err)
 	}
-	lon, err = strconv.ParseFloat(lonArg, 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return NewPoint(lat, lon, name)
-}
-
-// ParsePoints ...
-func ParsePoints(delimited string) []Point {
-	log.Debugf("Pasring points '%s' ...", delimited)
-	args := strings.Split(delimited, PointsDelimiter)
-	switch {
-	case args[0] == Helsinki:
-		return HelsinkiExamplePoints
-	case args[0] == Oxford:
-		return OxfordExamplePoints
-	default:
-		points := make([]Point, len(args))
-		for i, p := range args {
-			points[i] = ParsePoint(p)
-		}
-		return points
-	}
+	return lat64, lon64
 }
--- tool/tool_test.go
+++ tool/tool_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/bluest-eel/state/components"
 	"github.com/bluest-eel/state/tool"
+	"github.com/golang/geo/s1"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -18,8 +19,17 @@ func TestToolTestSuite(t *testing.T) {
 	suite.Run(t, &toolTestSuite{})
 }
 
+func (suite *toolTestSuite) TestParsePoint() {
+	result := tool.ParsePoint("51.7546135::-1.2577909::The White Horse")
+	suite.Equal("4876c6af5db009dd", result.CellID.ToToken())
+	suite.Equal(s1.Angle(0.9032884086721062), result.LatLon.Lat)
+	suite.Equal(s1.Angle(-0.021952592506622747), result.LatLon.Lng)
+	suite.Equal(51.7546135, result.LatLon.Lat.Degrees())
+	suite.Equal(-1.2577909, result.LatLon.Lng.Degrees())
+}
+
 func (suite *toolTestSuite) TestParsePoints() {
-	result := tool.ParsePoints(tool.OxfordPubs)
+	result := tool.ParsePoints(tool.OxfordPubExamplePoints)
 	names := make([]string, len(result))
 	for i, p := range result {
 		names[i] = p.Name
